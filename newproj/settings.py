785dda416736ebb507f099974ff4f861e43c5e9f
Erkaboy-coder <erkaboy756.5676@gmail.com>
2022-02-21 16:42:13 +0500

    changed

--- newproj/settings.py
+++ newproj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,7 @@ LOGIN_REDIRECT_URL = '/signin'
 LOGOUT_REDIRECT_URL = '/signin'
 DOCS_ROOT='topografiya/static/files/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- newproj/urls.py
+++ newproj/urls.py
@@ -4,14 +4,14 @@ from django.contrib import admin
 from django.conf import settings
 
 from django.views.static import serve
-from django.conf.urls.static import url
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('topografiya.urls')),
     path('', include('django.contrib.auth.urls')),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
 ]
 
 
